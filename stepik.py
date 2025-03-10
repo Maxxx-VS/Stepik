5371964f475ac49fa5f7e5e7f07a5ee3f3c79136
Maksim Vysochanskii <hot-zinc@yandex.ru>
2025-03-10 15:14:36 +0600

    Add files via upload

---
+++ stepik.py
@@ -0,0 +1,14 @@
+arr = []
+counter = int(input())
+
+for i in range(counter):
+    arr.append(int(input()))
+
+mul = int(input())
+
+# for jj in arr:
+#     for jjj in range(len(arr), 0):
+#         print (jj * arr[jjj])
+
+print(arr)
+print(arr.reverse())
